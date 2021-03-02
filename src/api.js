c0f33632878d57167893463c2a96261a18cbc2cb
RTurney <r.turney93@gmail.com>
2021-03-02 15:37:42 +0000

    Alter api.js to load document

--- src/api.js
+++ src/api.js
@@ -1,9 +1,28 @@
-function getArticle() {
+// function getArticle() {
+//   const guardianUrl = 'http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=http://worrydream.com/MediaForThinkingTheUnthinkable/note.html'
+//   fetch(guardianUrl)
+//   .then(response => response.json())
+//   .then(data => console.log(data))
+//
+// };
+
+function getArticleData() {
   const guardianUrl = 'http://news-summary-api.herokuapp.com/aylien?apiRequestUrl=https://api.aylien.com/api/v1/summarize?url=http://worrydream.com/MediaForThinkingTheUnthinkable/note.html'
-  fetch(guardianUrl)
-  .then(response => response.json())
-  .then(data => console.log(data))
+  return fetch(guardianUrl).then(response => {
+  return response.json();
+  })
+}
 
-};
+function renderPost(postData) {
+  let firstLine = postData.text.split('.')[0];
+  let remainingText = `<p>${postData.text}</p>`
+  let postHeadingHTML = `<h3>${firstLine}</h3>`;
+  return `${postHeadingHTML}${remainingText}`;
+}
 
-window.onload = getArticle();
+getArticleData().then(post => {
+  let rendered = renderPost(post);
+  document.getElementById("headlines").innerHTML = rendered;
+});
+
+window.onload = getArticleData();
